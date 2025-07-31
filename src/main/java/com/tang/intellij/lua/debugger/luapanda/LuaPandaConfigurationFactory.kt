89acd2ced3a518cb76b93ee27d79588e96ec9c14
qimeihong <qimeihong@tencent.com>
2025-07-31 17:43:28 +0800

    refactor(debugger): 将LuaPandaConfigurationFactory改为继承LuaConfigurationFactory
    修改LuaPandaConfigurationFactory的基类以复用公共逻辑，提升代码复用性

--- src/main/java/com/tang/intellij/lua/debugger/luapanda/LuaPandaConfigurationFactory.kt
+++ src/main/java/com/tang/intellij/lua/debugger/luapanda/LuaPandaConfigurationFactory.kt
@@ -16,13 +16,13 @@
 
 package com.tang.intellij.lua.debugger.luapanda
 
-import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.ConfigurationType
 import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.openapi.project.Project
+import com.tang.intellij.lua.debugger.LuaConfigurationFactory
 import com.tang.intellij.lua.lang.LuaIcons
 
-class LuaPandaConfigurationFactory(type: ConfigurationType) : ConfigurationFactory(type) {
+class LuaPandaConfigurationFactory(type: ConfigurationType) : LuaConfigurationFactory(type) {
     override fun createTemplateConfiguration(project: Project): RunConfiguration {
         return LuaPandaDebugConfiguration(project, this, "LuaPanda")
     }
